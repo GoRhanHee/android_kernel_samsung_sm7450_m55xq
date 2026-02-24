af8e3d8866e92f2509737aca3276fc87720b9a19
GoRhanHee <gorhanhee01@gmail.com>
2026-02-24 15:53:14 +0900

    workflow: add file upload code

--- build.sh
+++ build.sh
@@ -63,3 +63,7 @@ tar -xf "$TOOLCHAIN_FILE" -C kernel_platform && rm "$TOOLCHAIN_FILE"
 
 # Cooking Kernel
 ( env ${GKI_KERNEL_BUILD_OPTIONS} ${ANDROID_BUILD_TOP}/kernel_platform/build/android/prepare_vendor.sh ${CHIPSET_NAME} ${TARGET_PRODUCT} gki || exit 1)
+
+# Cooking Flashable File
+cp ./out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}/dist/boot.img ./boot.img
+tar -cvf m55xq_Kernel.tar boot.img
